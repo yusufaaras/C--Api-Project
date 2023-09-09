6dd5ea857f8bee862a0ab02413584b82ec7bd925
yusuf <arasy541@gmail.com>
2023-09-09 22:08:55 +0300

    Api
    Example Api

---
+++ ApiOgrenmeProjesi/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using ApiOgrenmeProjesi.Core;
+
+namespace ApiOgrenmeProjesi.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<ProductEntity> productEntities { get; set; }
+
+
+    }
+}
